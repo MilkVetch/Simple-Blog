cba4a372baf8964426c9786c49fa852579d1c389
milkvetch <457804000@qq.com>
2016-03-02 03:22:11 +1300

    responsive menu

--- js/script.js
+++ js/script.js
@@ -5,36 +5,79 @@ angular.module('blog',['ngAnimate', 'ui.router'])
 		$stateProvider
 			.state('home', {
 				url:'/',
-				templateUrl: 'static/home.html'
+				templateUrl: 'static/home.html',
+				data : { pageTitle: '芪聞異事' }
 			})
 			.state('note', {
 				url: '/note',
-				templateUrl:'static/note.html'
+				templateUrl:'static/note.html',
+				data : { pageTitle: '芪聞異事 - 隨筆' }
 			})
 			.state('front-end', {
 				url: '/front-end',
-				templateUrl:'static/front-end.html'
+				templateUrl:'static/front-end.html',
+				data : { pageTitle: '芪聞異事 - 前端' }
 			})
 			.state('design', {
 				url: '/design',
-				templateUrl:'static/design.html'
+				templateUrl:'static/design.html',
+				data : { pageTitle: '芪聞異事 - 設計' }
 			})
 			.state('novel', {
 				url: '/novel',
-				templateUrl:'static/novel.html'
+				templateUrl:'static/novel.html',
+				data : { pageTitle: '芪聞異事 - 小說' }
 			})
 			.state('music', {
 				url: '/music',
-				templateUrl:'static/music.html'
+				templateUrl:'static/music.html',
+				data : { pageTitle: '芪聞異事 - 音樂' }
 			})
 			.state('about', {
 				url: '/about',
-				templateUrl:'static/about.html'
+				templateUrl:'static/about.html',
+				data : { pageTitle: '芪聞異事 - 關於' }
 			});
 	})
 
 	.directive('headerNav', function(){
 		return {
-			templateUrl: 'module/headerNav.html'
+			templateUrl: 'module/headerNav.html',
+			controller: 'headerNav as menuList'
 		}
+	})
+
+	.directive('updateTitle', ['$rootScope', '$timeout',
+	  function($rootScope, $timeout) {
+	    return {
+	      link: function(scope, element) {
+
+	        var listener = function(event, toState) {
+
+	          var title = 'Default Title';
+	          if (toState.data && toState.data.pageTitle) title = toState.data.pageTitle;
+
+	          $timeout(function() {
+	            element.text(title);
+	          }, 0, false);
+	        };
+
+	        $rootScope.$on('$stateChangeSuccess', listener);
+	      }
+	    };
+	  }
+	])
+
+	.controller('headerNav', function() {
+		var menuList = this;
+		menuList.list = [
+			{page:'隨筆', url:'note'},
+			{page:'前端', url:'front-end'},
+			{page:'設計', url:'design'},
+			{page:'音樂', url:'music'},
+			{page:'小說', url:'novel'},
+			{page:'關於', url:'about'},
+		];
+		menuList.toggle = true;
 	});
+
